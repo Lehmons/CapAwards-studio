41da6b9ed0fb28c4167802f96519221c604db07b
Lehmons <freddiejackson217@gmail.com>
2022-12-14 11:26:41 +0000

    Update on Home

--- schemas/documents/home.js
+++ schemas/documents/home.js
@@ -7,12 +7,43 @@ export default {
       title: "Title",
       name: "title",
       type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: "Slug",
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) =>
+          `/${input.toLowerCase().replace(/\s+/g, "-").slice(0, 200)}`,
+      },
     },
     {
       title: "Blocks",
       name: "blocks",
       type: "array",
-      of: [{ type: "imageBlock" }],
+      of: [
+        { type: "imageBlock" },
+        { type: "textBlock" },
+        { type: "blueTextBlock" },
+        { type: "silverTextBlock" },
+        { type: "imageTextBlock" },
+        { type: "imageTextBlockBlue" },
+      ],
     },
   ],
+  preview: {
+    select: {
+      title: "title",
+      blocks: "blocks.length",
+    },
+    prepare({ title, blocks }) {
+      return {
+        title: title ? title : "No title defined",
+        subtitle: blocks ? `${blocks} blocks` : "No blocks defined",
+      };
+    },
+  },
 };
