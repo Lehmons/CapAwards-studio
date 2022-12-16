fd8614aee653af50eec8c761faa070d4e7ba40f9
Lehmons <freddiejackson217@gmail.com>
2022-12-16 16:09:46 +0000

    fixed quotes to normal text

--- schemas/objects/quoteBlock.js
+++ schemas/objects/quoteBlock.js
@@ -11,7 +11,7 @@ export default {
     {
       title: "Quote",
       name: "quote",
-      type: "headingOneTwoThreeNormalSmall",
+      type: "simple",
     },
     {
       title: "Attribution",
